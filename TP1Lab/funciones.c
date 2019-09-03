21f877663ad45134359d2782f507f79c9461a201
IvanRod <ivan010_rodriguez@hotmail.com>
2019-09-03 18:40:26 -0300

    Tp finalizado

--- TP1Lab/funciones.c
+++ TP1Lab/funciones.c
@@ -10,7 +10,10 @@ void menuOpciones()
     float resultadoDivision;
     int resultadoMultiplicacion;
     int opcion;
-    //int resultadoFactorial;
+    int factorialA;
+    int factorialB;
+
+
     do
 {
         mensajeInicio();
@@ -27,12 +30,16 @@ void menuOpciones()
         numeroB = pedirNumero("Ingrese segundo operando:");
         system("pause");
         system("cls");
+        break;
 
         case 3:
         resultadoSuma = sumarNumeros(numeroA,numeroB);
         resultadoResta = restarNumeros(numeroA,numeroB);
         resultadoMultiplicacion = multiplicarNumeros(numeroA,numeroB);
         resultadoDivision = dividirNumeros(numeroA,numeroB);
+        factorialA = factorial(numeroA);
+        factorialB = factorial(numeroB);
+        printf("Se realizaron las operaciones correctamente\n");
         system("pause");
         system("cls");
         break;
@@ -41,7 +48,17 @@ void menuOpciones()
         printf("El resultado de %d+%d es:%d\n",numeroA,numeroB,resultadoSuma);
         printf("El resultado de %d-%d es:%d\n",numeroA,numeroB,resultadoResta);
         printf("El resultado de %d*%d es:%d\n",numeroA,numeroB,resultadoMultiplicacion);
-        printf("El resultado de %d/%d es:%f\n",numeroA,numeroB,resultadoDivision);
+        if(numeroB != 0)
+        {
+            printf("El resultado de %d/%d es:%f\n",numeroA,numeroB,resultadoDivision);
+        }
+        else
+        {
+            printf("No se puede dividir %d entre 0\n",numeroA);
+        }
+        printf("El factorial de %d! es %d y el factorial de %d! es %d\n",numeroA,factorialA,numeroB,factorialB);
+
+
         system("pause");
         system("cls");
         break;
@@ -80,17 +97,23 @@ int multiplicarNumeros(int numA, int numB)
 float dividirNumeros(int numA,int numB)
 {
     float auxiliarResultado;
-    if (numB != 0)
-    {
-        auxiliarResultado = numA/(float)numB;
-        return auxiliarResultado;
-    }
-    else
-    {
-      printf("\nNo se puede dividir entre 0\n");
-    }
-    return 0;
+
+    auxiliarResultado = numA/(float)numB;
+    return auxiliarResultado;
+
 }
+unsigned int factorial(int numero)
+{
+    unsigned int resultado = 1;
+    int i;
+
+   for(i=numero;i>=1;i--)
+   {
+    resultado =  resultado * i;
+   }
+    return resultado;
+}
+
 void mensajeInicio(void)
 {
     printf("\t\tCalculadora C\t\t\n1.Elegir primer operando\n2.Elegir segundo operando\n3.Calcular las operaciones\n4.Mostrar las operaciones\n5.Salir\n");
--- TP1Lab/funciones.h
+++ TP1Lab/funciones.h
@@ -1,8 +1,59 @@
+/** \brief se encarga de sumar dos enteros
+ *
+ * \param int representa el primer operando de la suma
+ * \param  int representa el segundo operando de la suma
+ * \return int devuelve el resultado de la suma entre dos enteros
+ *
+ */
 int sumarNumeros (int,int);
+/** \brief se encarga de restar dos enteros
+ *
+ * \param int representa el primer operando de la resta
+ * \param int representa el segundo operando de la resta
+ * \return int devuelve el resultado de la resta entre dos enteros
+ *
+ */
 int restarNumeros(int,int);
+/** \brief se encarga de dividir dos enteros
+ *
+ * \param int es el dividendo de la division
+ * \param int es el divisor de la division
+ * \return float devuelve el resultado de la division entre dos enteros
+ *
+ */
 float dividirNumeros(int,int);
+/** \brief se encarga de multiplicar dos numeros
+ *
+ * \param int es el primer operando de la multiplicacion
+ * \param int es el segundo operando de la multiplicacion
+ * \return int devuelve el resultado de la multiplicacion entre dos enteros
+ *
+ */
 int multiplicarNumeros(int,int);
-int factorialNumero(int);
+/** \brief es el menu con todas las opciones pedidas
+ *
+ * \return void
+ *
+ */
 void menuOpciones();
+/** \brief pide un numero
+ *
+ * \param char* recibe un mensaje por puntero
+ * \return int devuelve un numero
+ *
+ */
 int pedirNumero(char*);
+/** \brief mensaje de bienvenida
+ *
+ * \param void
+ * \return void
+ *
+ */
 void mensajeInicio(void);
+/** \brief calcula el factorial de un numero
+ *
+ * \param int es el numero que se va a usar para calcular el factorial
+ * \return unsigned int devuelve el factorial de un numero
+ *
+ */
+unsigned int factorial(int);
